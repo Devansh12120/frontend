72dbf9b56f400b59942ff4577cc79e9eed0b551c
DEVANSH SHAH <56188496+Devansh12120@users.noreply.github.com>
2021-08-04 20:07:29 +0530

    Add files via upload

---
+++ connectme/frontend/index.php
@@ -0,0 +1,54 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Connect Me - Connect more</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Dosis:wght@300&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <section class="main-page">
+        <div class="left">
+            <div class="left-content">
+                <div>
+                    <i class="fa fa-search"></i>
+                    <div class="left-content-heading">
+                        Find your intrests
+                    </div>
+                </div>
+                <div>
+                    <i class="fa fa-user"></i>
+                    <div class="left-content-heading">
+                        Explore communities
+                    </div>
+                </div>
+                <div>
+                    <i class="fa fa-comment"></i>
+                    <div class="left-content-heading">
+                        Connect with people
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="right">
+            <div class="middle-content">
+                <i class="fa fa-share-square"></i>
+                <h1>See What's happening around the world right now</h1>
+                <h4>Join Share and Connect Now</h4>
+                <button href="signup.php" class="signin">Sign in</button>
+                <button href="login.php"  class="login">Login in</button>
+            </div>
+        </div>
+        <div class="footer"></div>
+    </section>
+</body>
+</html>
